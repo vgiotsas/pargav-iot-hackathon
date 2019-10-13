6ae64f287adaafcbfa4cb17211429c918bb39583
Petros Gigis <gkigkis@ics.forth.gr>
2019-10-13 13:58:37 +0200

    us data

--- convert_data/convert_to_timestamp.py
+++ convert_data/convert_to_timestamp.py
@@ -11,18 +11,18 @@ for filename in os.listdir(folder_path):
             csv_reader = csv.reader(csv_file, delimiter=',')
             line_count = 0
             for row in csv_reader:
-                if line_count == 0:
+                #if line_count == 0:
+                #    results.append(row)
+                #    line_count = 1
+                #    continue
+                #else:
+                try:
+                    date_time_obj = datetime.datetime.strptime(row[0], '%Y-%m-%d,%H:%M:%S')
+                    timestamp = int(datetime.datetime.timestamp(date_time_obj))
+                    row[0] = timestamp
                     results.append(row)
-                    line_count = 1
-                    continue
-                else:
-                    try:
-                        date_time_obj = datetime.datetime.strptime(row[0], '%Y-%m-%d,%H:%M:%S')
-                        timestamp = int(datetime.datetime.timestamp(date_time_obj))
-                        row[0] = timestamp
-                        results.append(row)
-                    except:
-                        pass
+                except:
+                    pass
         if len(results) > 1:
             with open(folder_path + "_timestamp/" + filename, mode='w') as results_file:
                 results_writer = csv.writer(results_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
